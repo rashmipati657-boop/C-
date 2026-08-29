f1cfa222def9528995f28f637753a48cfa0a2f45
Rashmi--pati <rashmipati657@gmail.com>
2026-08-29 18:07:01 +0530

    practics c language

---
+++ pattern printing.c/square.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() {
+    int x;
+    printf("Enter a number: ");
+    scanf("%d", &x);
+    for(int i=1;i<=;i++){
+        printf("1234");
+        printf("\n");
+    }
+    return 0;
+}
---
+++ patternprinting.c/pattern practics.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+ int main(){
+    int x;
+    printf("Enter your no. :");
+    scanf("%d",&x);
+    for(int i=1;i<=x;i++){
+     printf("1234\n");
+    }
+    return 0;
+
+}
